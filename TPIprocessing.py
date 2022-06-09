1ec3759ed36895eb207150a890c4484e47763007
slevinwyo <samuel.levin@wyo.gov>
2022-06-09 15:24:45 -0600

    Process TPI refactored as function

--- TPIprocessing.py
+++ TPIprocessing.py
@@ -9,40 +9,75 @@ samuel.levin@wyo.gov
 
 import arcpy
 import time
+from typing import Union
 
-start = time.perf_counter()
 
-arcpy.CheckOutExtension('Spatial')
+def processTPI(dem: Union[str, arcpy.sa.Raster],
+               outer_radius: int, inner_radius: int, unit: str,
+               out_tpi: str = None, mask: str = None):
+    """
+    Function to process topographic position index (TPI) from an input ditital elevation model (DEM).
+    Requires a DEM as input and path to the output TPI processed raster.
+    Also requires the inner and outer radius of the annulus (donut) neighborhood used to calculate TPI.
+    Unit should also be provided. Acceptable values include 'MAP' or 'CELL'.
+    MAP may be used for projected rasters with linear ground units.
+    CELL should be used for rasters using a geographic coordinate system.
+    A mask may be provided to limit the analysis are to within provided mask.
+    Implementation of methods described in Weiss, Topographic Position and Landforms Analysis, The Nature Conservancy.
+    http://www.jennessent.com/downloads/tpi-poster-tnc_18x22.pdf
 
-# Source DEM to calculate TPI from
-dem = '<PATH/TO/SOURCE/DEM>'
+    :param dem: Path to digital elevation model (DEM)
+    :param outer_radius: Outer radius of annulus (donut) neighborhood
+    :param inner_radius: Inner radius of annulus (donut) neighborhood
+    :param unit: Unit of outer and inner radius parameters. Acceptable values include 'MAP' or 'CELL'
+    :param out_tpi: Path to output TPI processed raster path. If provided, TPI will be saved to drive.
+    :param mask: Path to mask feature class or raster
+    :return: Output TPI processed raster
+    """
 
-# Output TPI processed raster
-out_tpi = '<PATH/TO/OUTPUT/TPI/RASTER>'
+    start = time.perf_counter()
 
-# Inner an outer radius of annulus neighborhood, units ('MAP' or 'CELL')
-outer_radius = 2500
-inner_radius = 2250
-unit = 'MAP'
+    arcpy.CheckOutExtension('Spatial')
 
-print('Calculating focal mean from annulus with {} meter OR, {} meter IR, {} meter sample swath..'.format(outer_radius,
-                                                                                                          inner_radius,
-                                                                                                          (outer_radius - inner_radius)))
+    # If input DEM is not already a raster object, cast as raster
+    dem_r = arcpy.Raster(dem) if dem != arcpy.sa.Raster else dem
 
-# Define the neighborhood and calculate the focal mean
-annulus = arcpy.sa.NbrAnnulus(inner_radius, outer_radius, unit)
-focal_mean = arcpy.sa.FocalStatistics(dem, annulus, 'MEAN', 'NODATA')
+    if mask:
+        arcpy.AddMessage('Setting mask environment..')
+        arcpy.env.mask = mask
+        dem_r = arcpy.sa.ExtractByMask(dem_r, mask)
+        arcpy.CalculateStatistics_management(dem_r)
 
-print('Subtracting focal mean from DEM..')
-dem_r = arcpy.Raster(dem)
-tpi = dem_r - focal_mean
+    spatial_ref = arcpy.Describe(dem_r).SpatialReference
+    if spatial_ref.type == 'Geographic' and unit == 'MAP':
+        arcpy.AddWarning('WARNING: MAP units specified with geographic coordinate system. Use CELL units.')
+    sr_unit = arcpy.Describe(dem_r).SpatialReference.linearUnitName
 
-print('Saving TPI to: {}'.format(out_tpi))
-tpi.save(out_tpi)
+    arcpy.AddMessage('Calculating focal mean from annulus with {0} {3} OR, {1} {3} IR, {2} {3} sample swath..'.format(
+        outer_radius,
+        inner_radius,
+        (outer_radius - inner_radius),
+        sr_unit.lower() if unit == 'MAP' else 'CELL'
+    ))
 
-print('Deleting intermediate rasters..')
-del focal_mean
-del dem_r
-del tpi
+    # Define the neighborhood and calculate the focal mean
+    annulus = arcpy.sa.NbrAnnulus(inner_radius, outer_radius, unit)
+    focal_mean = arcpy.sa.FocalStatistics(dem, annulus, 'MEAN', 'NODATA')
 
-print('RUN TIME: {} seconds'.format(time.perf_counter() - start))
+    arcpy.AddMessage('Subtracting focal mean from DEM..')
+    tpi = dem_r - focal_mean
+
+    if out_tpi:
+        arcpy.AddMessage('Saving TPI to: {}'.format(out_tpi))
+        tpi.save(out_tpi)
+
+    arcpy.AddMessage('Deleting intermediate rasters..')
+    del focal_mean
+    del dem_r
+
+    arcpy.AddMessage('PROCESS TPI RUN TIME: {} seconds'.format(time.perf_counter() - start))
+
+    # Reset mask so that further processing is not affected
+    arcpy.env.mask = None
+
+    return tpi
